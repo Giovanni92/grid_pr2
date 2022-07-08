092788da3c4c0c5a3888f46e948a5f7162a1b572
Giovanni92 <ivan_vasilevsky@yahoo.com>
2022-07-08 12:48:15 +0300

    new dates

--- js/script.js
+++ js/script.js
@@ -7,3 +7,14 @@ console.dir(document.body);
 
 currentMonthEl.innerText = 
 currentYear.toString()  + ' / ' + currentDate.toLocaleString('eng', {month : 'long'}) ;
+
+const firstDateOfMonth = new Date (currentDate);
+
+firstDateOfMonth.setDate(1);
+
+const lastDateOfMonth = new Date(firstDateOfMonth);
+lastDateOfMonth.setMonth(lastDateOfMonth.getMonth()+1);
+lastDateOfMonth.setDate(0);
+
+console.dir (firstDateOfMonth);
+console.dir (lastDateOfMonth);
