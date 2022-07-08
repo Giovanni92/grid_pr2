f6cfdd90545ffb458ee7d31ddaa78c8e1a95c7ff
Giovanni92 <ivan_vasilevsky@yahoo.com>
2022-07-08 12:10:12 +0300

    changes

--- js/script.js
+++ js/script.js
@@ -5,4 +5,5 @@ const currentMonthEl = document.querySelector('h2');
 
 console.dir(document.body);
 
-currentMonthEl.innerText = (currentYear.toString() + ' year') + ' / ' + currentMonth.toString();
+currentMonthEl.innerText = 
+currentYear.toString()  + ' / ' + currentDate.toLocaleString('eng', {month : 'long'}) ;
